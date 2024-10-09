7ef13b170015e6ae92d20bd325447c2171a60bca
Prahlada Mittal <prahlada_vm@es.iitr.ac.in>
2024-10-09 17:56:38 +1100

    Create taylor-test.py

---
+++ cartesian/taylor-test.py
@@ -0,0 +1,98 @@
+import numpy as np
+from gadopt import *
+from gadopt.inverse import *
+
+left_id, right_id, bottom_id, top_id = 1, 2, 3, 4  # Boundary IDs
+
+with CheckpointFile("forward_checkpoint.h5", mode="r") as fi:
+    mesh = fi.load_mesh(name="firedrake_default")
+    mesh.cartesian = True
+    dt_actual = fi.load_function(mesh, "Actual DT")
+    mu_actual = fi.load_function(mesh, "mu")
+
+
+V = VectorFunctionSpace(mesh, "CG", 2)  # Velocity function space (vector)
+W = FunctionSpace(mesh, "CG", 1)  # Pressure function space (scalar)
+Q = FunctionSpace(mesh, "CG", 2)  # Temperature function space (scalar)
+Q1 = FunctionSpace(mesh, "CG", 1)  # DT scalar function
+Z = MixedFunctionSpace([V, W])  # Mixed function space.
+
+z = Function(Z)  # A field over the mixed function space Z.
+u, p = split(z)  # Returns symbolic UFL expression for u and p
+z.subfunctions[0].rename("Velocity")
+z.subfunctions[1].rename("Pressure")
+
+Ra = Constant(1e5)  # Rayleigh number
+mu_control = Function(W, name="control log viscosity").assign(0.0)
+control = Control(mu_control)
+
+kx = 1.0  # Conductivity in the x direction
+ky = 0.0  # Conductivity in the y direction (set to zero for no diffusion)
+
+# Construct the anisotropic conductivity tensor
+# This tensor will have non-zero values only for the x component
+ex = as_vector((1, 0))  # Unit vector in the x direction
+ey = as_vector((0, 1))  # Unit vector in the y direction
+
+K = kx * outer(ex, ex) + ky * outer(ey, ey)
+
+smoother = DiffusiveSmoothingSolver(function_space=W, wavelength=1.0, K=K)
+mu = Function(W, name="viscosity")
+mu.project(10 ** smoother.action(mu_control))
+approximation = BoussinesqApproximation(Ra)
+
+X = SpatialCoordinate(mesh)
+T_f = Function(Q, name="T").interpolate((1.0-X[1]) + (0.05*cos(pi*X[0])*sin(pi*X[1])))
+T_check = conditional(T_f > 0, T_f, 0)
+T = Function(Q, name="Temperature").interpolate(T_check)
+
+Z_nullspace = create_stokes_nullspace(Z, closed=True, rotational=False)
+
+stokes_bcs = {
+    bottom_id: {'uy': 0},
+    top_id: {'uy': 0},
+    left_id: {'ux': 0},
+    right_id: {'ux': 0},
+}
+
+temp_bcs = {
+    bottom_id: {'T': 1.0},
+    top_id: {'T': 0.0},
+}
+
+stokes_solver = StokesSolver(z, T, approximation, bcs=stokes_bcs, mu=mu,
+                             nullspace=Z_nullspace, transpose_nullspace=Z_nullspace,
+                             )
+
+surface_force_solver = BoundaryNormalStressSolver(stokes_solver, top_id)
+
+# Solve Stokes sytem:
+stokes_solver.solve()
+
+# compute `model` dynamic topography
+surface_force = surface_force_solver.solve()
+
+#dynamic topography
+deltarho_g = Constant(1e3) #delta rho = 100, g = 10
+dt_iso = Function(Q1, name="Iso DT")
+dt_iso.interpolate((surface_force / deltarho_g))
+
+# form the objective function, between model and `data`
+objective = assemble(0.5 * (dt_actual - dt_iso) ** 2 * ds(top_id))
+
+print("J=", objective)
+
+# Defining the reduced functional
+reduced_functional = ReducedFunctional(objective, controls=control)
+der_func = reduced_functional.derivative(options={"riesz_representation": "L2"})
+der_func.rename("derivative")
+
+# Visualising the derivative
+VTKFile("inverse-example.pvd").write(*z.subfunctions, T, dt_actual, dt_iso, der_func, mu_actual)
+
+# Performing taylor test
+Delta_mu = Function(mu_control.function_space(), name="Delta_Temperature")
+Delta_mu.dat.data[:] = np.random.random(Delta_mu.dat.data.shape)
+
+# Perform the Taylor test to verify the gradients
+minconv = taylor_test(reduced_functional, mu_control, Delta_mu)
